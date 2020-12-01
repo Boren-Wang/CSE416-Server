8ef83aba3f57e93b9535c1551d0c42737f380a44
Boren <borwang@cs.stonybrook.edu>
2020-12-01 22:17:16 +0800

    Updated seawulf.slurm

--- src/main/resources/trigger.sh
+++ src/main/resources/trigger.sh
@@ -1,1 +1,1 @@
-cat src/main/resources/seawulf.slurm | sshpass -p "mhw1015sz15" ssh -o StrictHostKeyChecking=no borwang@login.seawulf.stonybrook.edu 'source /etc/profile.d/modules.sh; module load slurm; module load anaconda/3; cd /gpfs/scratch/borwang/test_ssh_submit; sbatch'
+cat src/main/resources/seawulf.slurm | sshpass -p "mhw1015sz15" ssh -o StrictHostKeyChecking=no borwang@login.seawulf.stonybrook.edu 'source /etc/profile.d/modules.sh; module load slurm; module load anaconda/3; module load mpi4py/3.0.3; cd /gpfs/scratch/borwang/test_ssh_submit; sbatch'
