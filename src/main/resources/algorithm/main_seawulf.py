ad4be44edd3f1fc737913b638308a5ceeb615da5
Boren <borwang@cs.stonybrook.edu>
2020-12-12 13:41:11 +0800

    完善了自动命名机制

--- src/main/resources/algorithm/main_seawulf.py
+++ src/main/resources/algorithm/main_seawulf.py
@@ -8,7 +8,7 @@ from redistricting import redistricting
 from seed import generateSeed
 from graph import Graph, Node
 
-iterationLimit = 50  # 测试速度或者会不会报错的时候改成50，实际run的时候改成1000
+iterationLimit = 1000  # 测试速度或者会不会报错的时候改成50，实际run的时候改成1000
 
 GA = './algorithm/GA.json' # 测试速度或者会不会报错的时候用ga速度最快
 districtsGA = 14
